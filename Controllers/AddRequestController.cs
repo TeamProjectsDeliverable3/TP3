b6feb400fdfa18f1a1b5359514b94c4c91638cd0
melanie <m.anderson-12@student.lboro.ac.uk>
2014-03-19 18:27:22 +0000

    Timetabling System Version 1
    First Version of timetabling system.

---
+++ Controllers/AddRequestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimetablingSystem1.Controllers
+{
+    public class AddRequestController : Controller
+    {
+        //
+        // GET: /AddRequest/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ DataAccess/DataAccessLayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TimetablingSystem1.DataAccess
+{
+    public class DataAccessLayer
+    {
+       //static SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["team02database"].ConnectionString.ToString());
+        static SqlConnection conn = new SqlConnection("Data Source=co-web.lboro.ac.uk;Integrated Security=False;User ID=team02;Password=s9s38vb;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False");
+        
+
+       
+
+        public static bool UserIsValid(string username, string password)
+        {
+            
+                bool authenticated = false;
+
+                string query = string.Format("SELECT * FROM TestLogin WHERE Username = '{0}' AND Password = '{1}'", username, password);
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                authenticated = sdr.HasRows;
+                conn.Close();
+                return (authenticated);
+
+                          
+            
+
+        }
+    }
+}
